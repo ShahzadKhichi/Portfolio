0e9a1d5148badb983cd5da5ab047b65c675090bc
ShahzadKhichi <shahzadkhichi996@gmail.com>
2025-07-24 14:25:43 +0500

    project button positoni fixed

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -34,8 +34,8 @@ const Home = () => {
           }`}
       >
         <Navbar></Navbar>
-        <div className="w-[100vw] lg:w-full h-fit lg:h-[70vh]  flex flex-col-reverse items-center sm:flex-col-reverse  lg:flex-row lg:justify-center lg:items-start gap-10 lg:gap-30 pb-8 pl-4  lg:mt-40 mt-10">
-          <div className="flex lg:flex-none flex-col justify-center h-fit items-center lg:w-[500px] lg:gap-10 lg:h-fit bg-transparent ">
+        <div className="w-[100vw] lg:w-full h-fit lg:h-[74vh]  flex flex-col-reverse items-center sm:flex-col-reverse  lg:flex-row lg:justify-center lg:items-start gap-10 lg:gap-30 pb-8 pl-4  lg:mt-40 mt-10">
+          <div className="flex lg:flex-none  flex-col justify-center h-fit items-center lg:w-[500px] lg:gap-10 lg:h-fit  ">
             <h4 className="text-white font-extrabold text-2xl lg:text-[45px] font-mono inline-flex animate-pulse   ">
               Shahzad Khichi
             </h4>
@@ -56,13 +56,13 @@ const Home = () => {
               <span className="text-blue-300">{"</>"}</span>
               <SideBar></SideBar>
             </h6>
-            <div className="h-fit lg:w-[50%] w-[40%] flex items-center  lg:mt-30 mt-20 flex-col  gap-4 ">
+            <div className="h-fit z-10 lg:w-[50%] lg:ml-96 w-[40%] flex items-center  lg:mt-30 mt-20 flex-col  gap-4 ">
               <button className="lg:mx-13  w-fit hover:scale-105 transition-all duration-200 flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
                 <span className="  font-extrabold p  font-mono hover:scale-105  px-10 py-3.5 transition-all ease-in duration-200 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
                   Projects
                 </span>
               </button>
-              <HiOutlineArrowNarrowDown className="text-amber-50 font-bold w-10 h-8  " />
+              <HiOutlineArrowNarrowDown className="text-amber-50 font-bold w-17 h-10  " />
             </div>
           </div>
 
@@ -76,7 +76,7 @@ const Home = () => {
 
       {/* This is the projects section */}
       <div
-        className={`lg:w-full w-[100vw]     transition-all duration-300 bg-[#000000f3] relative gap-20 overflow-x-hidden 
+        className={`lg:w-full w-[100vw] h-[10vh]    transition-all duration-300 bg-[#000000f3] relative gap-20 overflow-x-hidden 
           }`}
       ></div>
     </>
