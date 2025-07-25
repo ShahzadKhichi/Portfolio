a703f0c4e81edc1fbb87dbc1e357dff2840b56f6
ShahzadKhichi <shahzadkhichi996@gmail.com>
2025-07-25 11:58:36 +0500

    vercel file added

--- Backend/index.js
+++ Backend/index.js
@@ -8,22 +8,19 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+app.use(express.static("/home/msk/Desktop/vs_code/Portfolio/Frontend/dist"));
 
 // app.use("/api/Admin");
 // app.use("api/user");
-app.get("/", (req, res) => {
-  console.log("here");
-
-  res.sendFile(
-    "/home/msk/Desktop/vs_code/Portfolio/Frontend/dist/index.html",
-    () => {
-      res.send("falied to get file");
-    }
-  );
+app.get("*", (req, res) => {
+  res.status(200).json({
+    message: "Hello from server",
+    success: true,
+  });
 });
 (async () => {
   await db_connect();
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`server started on port:${PORT}`);
   });
 })();
