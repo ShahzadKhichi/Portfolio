a3f84cceb41074be1c364581eff84ecdc7ee9b69
ShahzadKhichi <shahzadkhichi996@gmail.com>
2025-07-25 20:54:58 +0500

    latest updates

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import image from "../assets/me.jpg";
 
 const Navbar = () => {
   return (
-    <div className="text-white w-full flex items-center top-0 sticky z-10 bg-[#000000f3]  lg:justify-center justify-around  lg:pt-6 py-4 ">
+    <div className="text-white w-full flex items-center top-0 sticky z-10  bg-[#000000f3]   lg:justify-center justify-around  lg:pt-6 pt-4 ">
       <div className="lg:w-[35%] outline-1  outline-blue-400 w-fit p-3 rounded-l-2xl rounded-r-2xl justify-around flex items-center  gap-2 lg:text-lg text-[10px]  bg-[#262627d2] pr-10 ">
         <Link
           to="/#main"
